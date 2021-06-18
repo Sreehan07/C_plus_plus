67c356d2cb0b058b70335b637199aefaae590f91
Sreehan07 <82800385+Sreehan07@users.noreply.github.com>
2021-06-18 14:00:28 +0530

    Create method_overriding.cpp

---
+++ method_overriding.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class lol
+{
+    public:
+    int android()
+    {
+        cout<<"this is android 9th version";
+        return 0;
+    }   
+};
+class ball:lol
+{
+    public:
+    int android()
+    {
+        cout<<"this is android 10th version";
+        return 0;
+    }
+};
+int main()
+{
+    class lol object;
+    class ball object2;
+    object2.android();
+    object.android();
+    return 0;
+}
